32bfc7058a9f07c1fad2c5768bbd6da0c66035a2
rosajong <rosa@akoten.com>
2015-11-12 13:06:49 +0100

    Added something random

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    something_random = models.BooleanField(default=True)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
